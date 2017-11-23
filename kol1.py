84731c288bdc8022e934079c4c3166a5f9e1e64b
Paulina Olejniczak <olejniczakpp@gmail.com>
2017-11-23 19:34:35 +0100

    changes

--- kol1.py
+++ kol1.py
@@ -16,59 +16,79 @@
 
 #!/usr/bin/env python2.7
 from random import gauss
+from time import sleep
 
-fname="data.txt"
-number=0
+fname = "data.txt"
+number = 0
 
 
 class Plane:
-    def __init__(self):
-        self.orientation=0
+    def __init__(self, type_of_plane):
+        self.type_of_the_plane = type_of_plane
+
+    def get_name_of_the_plane(self):
+        print(self.type_of_the_plane)
+
+
+class Simulation(Plane):
+    def __init__(self, type_of_plane):
+        Plane.__init__(self, type_of_plane)
+        self.orientation_of_the_plane = 0
 
     def turbulation(self):
-        self.orientation = gauss(20, 15)
+        self.orientation_of_the_plane = gauss(20, 15)
 
-    def correction(self):
-        self.orientation -= (self.orientation / 4)
+    def tilt_correction(self):
+        if self.orientation_of_the_plane > 25:
+            self.orientation_of_the_plane = self.orientation_of_the_plane - \
+                                            (((self.orientation_of_the_plane / 360) / 10) * 360)
+        else:
+            self.orientation_of_the_plane = self.orientation_of_the_plane - \
+                                            (((self.orientation_of_the_plane / 360) / 5) * 360)
 
-
-
-if __name__=="__main__":
-
-  plane_1=Plane()
-
-  while (1):
-    choice1 = raw_input("The plane takes off, please choose from below:\n1 - Start the simulation of the flight.\n2 - Exit.\n")
-    if(choice1=="1" or choice1=="2" ):
-      break
-    else:
-      print("Wrong input")
-      continue
+    def get_orientation_of_the_plane(self):
+        print(self.orientation_of_the_plane)
 
 
-  file=open(fname,'w+')
-  file.truncate()
+if __name__ == "__main__":
 
-  if(choice1=="1"):
-    while(1):
-      number+=1
-      plane_1.turbulation()
-      print('Orientation of plane = ', plane_1.orientation)
-      file.write(str(number) + '. Orientation = ' + str(plane_1.orientation))
-      plane_1.correction()
-      print('After correction, orientation =', plane_1.orientation)
-      file.write(', after correlation = '+str(plane_1.orientation)+'\n')
-      while(1):
-        s = raw_input("\n1 - Continue.\n2 - Exit.\n")
-        if(s=="1" or s=="2"):
-          break
-          print("Wrong input")
-        if(s!="1" or s!="2"):
-          continue
-      if (s=="2"):
-        print("Goodbye\nAll data is saved in file - data.txt.")
-        break
+    file1 = open(fname, 'w+')
+    file1.truncate()
 
+    plane_1 = Simulation("88")
+    while 1:
+        choice1 = raw_input("The plane takes off, please choose from below:\n1 - "
+                            "Start the simulation of the flight.\n2 - Exit.\n")
+        if choice1 == "1" or choice1 == "2":
+            break
+        else:
+            print("Wrong input")
+            continue
 
-  if(choice1=="2"):
-    print("Goodbye!\nAll data is saved in file - data.txt.")
+    if choice1 == "1":
+        type_plane = raw_input("Please enter the type of the plane.\n")
+        plane_1.type_of_the_plane = type_plane
+        while 1:
+            number += 1
+            plane_1.turbulation()
+            print("Current information: ")
+            plane_1.get_orientation_of_the_plane()
+            print("\n")
+            file1.write(str(number) + '. Orientation = ' + str(plane_1.orientation_of_the_plane))
+            plane_1.tilt_correction()
+            print("After correction: ")
+            plane_1.get_orientation_of_the_plane()
+            file1.write(', after correction = ' + str(plane_1.orientation_of_the_plane) + '\n')
+            while 1:
+                s = raw_input("\n1 - Continue.\n2 - Exit.\n")
+                if s == "1" or s == "2":
+                    break
+                print("Wrong raw_input")
+                if s != "1" or s != "2":
+                    continue
+            if s == "2":
+                print("Goodbye\nAll data is saved in file - data.txt.")
+                break
+
+    if choice1 == "2":
+        print('Goodbye!\nAll data is saved in file - data.txt.')
